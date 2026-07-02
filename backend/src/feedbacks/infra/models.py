0daabff40e34dac15b51bef237ae5431e2587aae
Romaskaa <topsheat@gmail.com>
2026-07-02 13:27:14 +0500

    настроенный mapper + sql repo

--- backend/src/feedbacks/infra/models.py
+++ backend/src/feedbacks/infra/models.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 
-from sqlalchemy import UniqueConstraint, ForeignKey, UniqueContraint
+from sqlalchemy import CheckConstraint, ForeignKey, UniqueConstraint
 from sqlalchemy import Mapped, mapped_column
 
 from src.core.database import Base
@@ -22,6 +22,6 @@ class FeedbackOrm(Base):
     comment: Mapped[str | None] = mapped_column(nullable=True)
 
     __table_args__ = (
-        UniqueContraint("ticket_id", name="uq_feedbacks_ticket_id"),
-        UniqueConstraint("rating >= 1 AND rating <= 5", name="ck_feedbacks_rating_range"),
+        UniqueConstraint("ticket_id", name="uq_feedbacks_ticket_id"),
+        CheckConstraint("rating >= 1 AND rating <= 5", name="ck_feedbacks_rating_range"),
     )
